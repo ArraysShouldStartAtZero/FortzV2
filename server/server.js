3435e9fa929617e4bec90f60f46868dd2d02f5f1
AndrewGraber <graber15@purdue.edu>
2018-04-24 17:32:07 -0400

    Basic server code serving up static html page

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var express = require('expresss');
+var sock = require('socket.io');
+
+var app = express();
+var port = 3000;
+var server = app.listen(port, function() {
+  console.log('Listening for requests on port ' + port);
+});
+
+app.use(express.static('../client/'));
+
+var io = sock(server);
+
+console.log('Setting up the Socket Handler');
+
+io.on('connection', function(socket) {
+  console.log('Client Connected (id: ' + socket.id + ')');
+  //TODO messaging setup here
+});
+
+console.log('Socket Handler Setup Complete');
